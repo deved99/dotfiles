d7c5ec2541453489c6e691557f723fd0d2038534
Davide Forlani <forla.df@gmail.com>
2022-05-08 11:39:50 +0200

    Changed some settings in org-mode.

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -1,11 +1,11 @@
-;; [[file:~/.dotfiles/files/emacs/init.org::*Summary][Summary:1]]
-;; [[[[file:~/.dotfiles/files/emacs/init.org::prepare/garbage-collection][prepare/garbage-collection]]][prepare/garbage-collection]]
+;; [[file:init.org::*Summary][Summary:1]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::prepare/garbage-collection][prepare/garbage-collection]]][prepare/garbage-collection]]
 (setq gc-cons-threshold 100000000)
 ;; prepare/garbage-collection ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::*use-package setup][use-package setup]]][]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::*Summary][Summary]]][]]
 (setq package-enable-at-startup nil)
 ;; ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::*use-package setup][use-package setup]]][]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::*Summary][Summary]]][]]
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -19,13 +19,13 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 ;; ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::*use-package setup][use-package setup]]][]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::*Summary][Summary]]][]]
 (straight-use-package 'use-package)
 ;; ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::*use-package setup][use-package setup]]][]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::*Summary][Summary]]][]]
 (setq straight-use-package-by-default t)
 ;; ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::prepare/startup-time][prepare/startup-time]]][prepare/startup-time]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::prepare/startup-time][prepare/startup-time]]][prepare/startup-time]]
 (add-hook 'emacs-startup-hook
   (lambda ()
     (message "Emacs ready in %s with %d garbage collections."
@@ -34,34 +34,34 @@
 	  (time-subtract after-init-time before-init-time)))
        gcs-done)))
 ;; prepare/startup-time ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::*cleanup][cleanup]]][]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::*Summary][Summary]]][]]
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 (tooltip-mode 0)
 ;; ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::*cleanup][cleanup]]][]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::*Summary][Summary]]][]]
 (fringe-mode 10)
 ;; ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::*cleanup][cleanup]]][]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::*Summary][Summary]]][]]
 (set-default 'truncate-lines t)
 ;; ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::visual/keeparound][visual/keeparound]]][visual/keeparound]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::visual/keeparound][visual/keeparound]]][visual/keeparound]]
 (setq scroll-margin 5
       scroll-conservatively 10000)
 ;; visual/keeparound ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::visual/fonts][visual/fonts]]][visual/fonts]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::visual/fonts][visual/fonts]]][visual/fonts]]
 (add-to-list 'default-frame-alist
 	     '(font . "JetBrains Mono 15"))
 (set-fontset-font t 'symbol "Noto Color Emoji" nil 'append)
 ;; visual/fonts ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::visual/theme][visual/theme]]][visual/theme]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::visual/theme][visual/theme]]][visual/theme]]
 (use-package base16-theme
   :config
   (setq base16-distinct-fringe-background nil)
   (load-theme 'base16-google-dark t))
 ;; visual/theme ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::visual/modeline][visual/modeline]]][visual/modeline]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::visual/modeline][visual/modeline]]][visual/modeline]]
 (defun get_current_state ()
   (cond
    ((eq evil-state 'normal) "NORMAL")
@@ -84,7 +84,7 @@
 	       '(:eval (is_modified))
 	       " (%m)"))
 ;; visual/modeline ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::evil/main][evil/main]]][evil/main]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::evil/main][evil/main]]][evil/main]]
 (use-package evil
   :after undo-tree
   :init
@@ -97,76 +97,77 @@
   (setq evil-echo-state nil)
   :config (evil-mode 1))
 ;; evil/main ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::*evil integrations][evil integrations]]][]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::*Summary][Summary]]][]]
 (use-package evil-collection
   :after evil
   :init (evil-collection-init))
 ;; ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::*evil integrations][evil integrations]]][]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::*Summary][Summary]]][]]
 (use-package evil-org
+  :init
+  <<evil/integrations/options>>
   :config
   (require 'evil-org-agenda)
   (evil-org-agenda-set-keys))
 ;; ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::*vim plugins][vim plugins]]][]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::*Summary][Summary]]][]]
 (use-package evil-surround
   :after evil
   :config (global-evil-surround-mode 1))
 ;; ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::*vim plugins][vim plugins]]][]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::*Summary][Summary]]][]]
 (use-package evil-commentary
   :after evil
   :config (evil-commentary-mode))
 ;; ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::*vim plugins][vim plugins]]][]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::*Summary][Summary]]][]]
 (use-package evil-indent-plus
   :after evil
   :init (evil-indent-plus-default-bindings))
 ;; ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::*vim plugins][vim plugins]]][]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::*Summary][Summary]]][]]
 (use-package evil-numbers
   :after evil
   :config
   (evil-define-key '(normal visual) 'global (kbd "SPC +") 'evil-numbers/inc-at-pt)
   (evil-define-key '(normal visual) 'global (kbd "SPC -") 'evil-numbers/dec-at-pt))
 ;; ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::evil/undo][evil/undo]]][evil/undo]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::evil/undo][evil/undo]]][evil/undo]]
 (use-package undo-tree
   :init
   ;; (setq undo-tree-auto-save-history (concat user-emacs-directory "undo"))
   :config (global-undo-tree-mode))
 ;; evil/undo ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::*org mode][org mode]]][]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::*Summary][Summary]]][]]
+(setq org-startup-indented t
+      org-startup-folded t)
 (use-package org
   :after evil-org
-  :init
-  (setq org-startup-indented t
-        org-startup-folded t)
   :config (add-hook 'org-mode-hook 'evil-org-mode))
 ;; ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::*org mode][org mode]]][]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::*Summary][Summary]]][]]
 (use-package ox-gfm)
 (setq org-export-backends '(html latex ox-gfm))
 ;; ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::misc/startup-file][misc/startup-file]]][misc/startup-file]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::misc/startup-file][misc/startup-file]]][misc/startup-file]]
 (setq inhibit-startup-screen t
       initial-buffer-choice "~/current.org")
 ;; misc/startup-file ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::misc/autoclose-parenthesis][misc/autoclose-parenthesis]]][misc/autoclose-parenthesis]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::misc/autoclose-parenthesis][misc/autoclose-parenthesis]]][misc/autoclose-parenthesis]]
 (electric-pair-mode)
 ;; misc/autoclose-parenthesis ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::misc/backup-files][misc/backup-files]]][misc/backup-files]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::misc/backup-files][misc/backup-files]]][misc/backup-files]]
 (setq backup-dir (concat user-emacs-directory "backups")
       backup-directory-alist `(("." . ,backup-dir))
       auto-save-file-name-transforms `((".*", backup-dir t)))
 ;; misc/backup-files ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::misc/expandtab][misc/expandtab]]][misc/expandtab]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::misc/expandtab][misc/expandtab]]][misc/expandtab]]
 (setq-default indent-tabs-mode nil)
 ;; misc/expandtab ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::misc/rainbow-mode][misc/rainbow-mode]]][misc/rainbow-mode]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::misc/rainbow-mode][misc/rainbow-mode]]][misc/rainbow-mode]]
 (use-package rainbow-mode)
 ;; misc/rainbow-mode ends here
-;; [[[[file:~/.dotfiles/files/emacs/init.org::misc/filetypes][misc/filetypes]]][misc/filetypes]]
+;; [[[[file:~/Projects/dotfiles/files/emacs/init.org::misc/filetypes][misc/filetypes]]][misc/filetypes]]
 (use-package rust-mode)
 (use-package lua-mode)
 (use-package json-mode)
