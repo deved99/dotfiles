1287b4c29114f2efbb9c69efa45af9bf475fe440
Davide Forlani <davide.forlani@overweb.it>
2022-08-25 07:57:44 +0200

    Fixed Emacs refreshing packages on start, Company now always manual.

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -219,7 +219,7 @@
              "odd" 'org-deadline)
   ;; ends here
   ;; :straight (:type built-in)
-  )
+  :pin manual)
 ;; [[[[file:~/.dotfiles/files/emacs/init.org::note-taking][note-taking]]][]]
 (use-package org-tempo
   :ensure org
@@ -309,8 +309,7 @@
 ;; ends here
 ;; [[[[file:~/.dotfiles/files/emacs/init.org::programming/company][programming/company]]][programming/company]]
 (use-package company
-  :hook ((after-init . global-company-mode)
-         (prog-mode . (lambda () (setq-local company-idle-delay 0.4))))
+  :hook (after-init . global-company-mode)
   :general
   (general-imap "C-n" 'company-select-next
                 "C-p" 'company-select-previous)
@@ -368,16 +367,3 @@
 (setq vc-follow-symlinks t)
 ;; ends here
 ;; Summary 🗂️:1 ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(org yaml-mode which-key visual-fill-column vertico use-package undo-tree rust-mode rainbow-mode rainbow-delimiters plantuml-mode php-mode ox-gfm org-superstar org-roam-ui org-appear orderless nix-mode marginalia magit lua-mode lsp-pyright json-mode jinja2-mode haskell-mode general evil-surround evil-org evil-numbers evil-commentary evil-collection edit-indirect doom-themes doom-modeline dockerfile-mode consult company-box all-the-icons adaptive-wrap)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
