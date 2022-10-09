57a5a02e27ed248fa380d12bf7d896730be5e7b7
Davide Forlani <davide.forlani@overweb.it>
2022-10-09 15:25:46 +0200

    Added treesitter.

--- files/neovim/init.lua
+++ files/neovim/init.lua
@@ -1,5 +1,45 @@
 -- [[file:init.org::*Summary][Summary:1]]
 -- [[[[file:~/.dotfiles/files/neovim/init.org::*Summary][Summary]]][]]
-vim.opt.number = true
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+-- ends here
+-- [[[[file:~/.dotfiles/files/neovim/init.org::*Summary][Summary]]][]]
+require('packer').startup(function(use)
+  -- [[[[file:~/.dotfiles/files/neovim/init.org::*Summary][Summary]]][]]
+  use 'wbthomason/packer.nvim'
+  -- ends here
+  -- [[[[file:~/.dotfiles/files/neovim/init.org::*Summary][Summary]]][]]
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  -- ends here
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+-- ends here
+-- [[[[file:~/.dotfiles/files/neovim/init.org::*Summary][Summary]]][]]
+require('nvim-treesitter.configs').setup {
+  -- Ensure these are installed:
+  ensure_installed = { "c", "lua", "rust" },
+  sync_install = false,
+  auto_install = true,
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+  },
+}
 -- ends here
 -- Summary:1 ends here
