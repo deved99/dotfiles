951158a8a829c68c60b0f22d47839549850e4aa3
Davide Forlani <davide.forlani@overweb.it>
2023-02-11 14:29:22 +0100

    Disabled vim treesitter since it caused errors.

--- files/neovim/lua/lsp/treesitter.lua
+++ files/neovim/lua/lsp/treesitter.lua
@@ -1,8 +1,11 @@
 local config = function()
     require('nvim-treesitter.configs').setup {
-      -- ensure_installed = { "lua", "python", "rust" },
-      -- auto_install = true,
-      highlight = { enable = true },
+      ensure_installed = { "lua", "python", "rust" },
+      auto_install = true,
+      highlight = { 
+          enable = true,
+          disable = {"vim"},
+      },
     }
 end
 
--- roles/program.neovim/files/rosepine.lua
+++ roles/program.neovim/files/rosepine.lua
@@ -1,10 +1,7 @@
--- Aliased to avoid Treesitter error
-local vim_cmd = vim.cmd
-
 return {
     'rose-pine/neovim', 
     name = 'rose-pine', 
     config = function() 
-        vim_cmd('colorscheme rose-pine') 
+        vim.cmd('colorscheme rose-pine') 
     end
 }
