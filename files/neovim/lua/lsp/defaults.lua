bcb8327f0b29f8276ac16c410ff506b0004d2e42
Davide Forlani <davide.forlani@overweb.it>
2023-02-12 19:47:42 +0100

    Added custom settings for Lua lsp.

---
+++ files/neovim/lua/lsp/defaults.lua
@@ -0,0 +1,40 @@
+local keybindings = function(_, bufnr)
+    local keybindings = {
+        -- goto
+        gd = vim.lsp.buf.definition,
+        gD = vim.lsp.buf.declaration,
+        K = vim.lsp.buf.hover,
+        ["<leader>lr"] = vim.lsp.buf.rename,
+    }
+
+    for keys, func in pairs(keybindings) do
+        vim.keymap.set('n', keys, func, { buffer = bufnr })
+    end
+end
+
+local options = function()
+    return {
+        on_attach = function(_, bufnr)
+            keybindings(_, bufnr)
+        end
+    }
+end
+
+-- Could this be merged in the function above?
+local add_coq_capabilities = function(lsp_options)
+    local coq = require('coq')
+    return coq.lsp_ensure_capabilities(lsp_options)
+end
+
+local handler = function (server_name)
+    local lspconfig = require("lspconfig")
+
+    local lsp_options = options()
+    lsp_options = add_coq_capabilities(lsp_options)
+    lspconfig[server_name].setup(lsp_options)
+end
+
+return {
+    handler = handler,
+    options = options
+}
--- files/neovim/lua/lsp/mason.lua
+++ files/neovim/lua/lsp/mason.lua
@@ -1,51 +1,37 @@
-local set_lsp_keybindings = function(_, bufnr)
-    local keybindings = {
-        gd = vim.lsp.buf.definition,
-        ["<leader>lr"] = vim.lsp.buf.rename,
+local defaults = require("lsp.defaults")
+
+local lua_handler = function()
+    local lsp_options = defaults.options()
+    lsp_options.settings = {
+        Lua = {
+          runtime = {
+            version = 'LuaJIT',
+          },
+          diagnostics = {
+            globals = {'vim'},
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          telemetry = {
+            enable = false,
+          },
+        },
     }
-    -- nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-    -- nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-    -- nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-    -- nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-    -- nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-    -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-    -- -- See `:help K` for why this keymap
-    -- nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-    -- nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-    --
-    -- -- Lesser used LSP functionality
-    -- nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-    -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-    -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-    for keys, func in pairs(keybindings) do
-        vim.keymap.set('n', keys, func, { buffer = bufnr })
-    end
-end
-
-local lsp_default_handler = function (server_name)
-    local coq = require('coq')
-    local lspconfig = require("lspconfig")
-
-
-    local lsp_options = {
-        on_attach = function(_, bufnr)
-            set_lsp_keybindings(_, bufnr)
-        end
-    }
-    lsp_options = coq.lsp_ensure_capabilities(lsp_options)
-    lspconfig[server_name].setup(lsp_options)
+    require'lspconfig'.lua_ls.setup(lsp_options)
 end
 
 local config = function()
     -- setup mason
     require("mason").setup()
     require("mason-lspconfig").setup {
-        ensure_installed = { "rust_analyzer", "pyright" }
+        ensure_installed = { "rust_analyzer", "pyright", "lua_ls" }
     }
 
     -- see `help mason-lspconfig-automatic-server-setup`.
     require("mason-lspconfig").setup_handlers {
-        lsp_default_handler
+        defaults.handler,
+        lua_ls = lua_handler
     }
 end
 
