083094459f0dcaa7ad9894fd938ef0b1fd8f6a2d
Davide Forlani <davide.forlani@overweb.it>
2023-02-15 14:06:17 +0100

    Configured telescope-fzf-native.

--- files/neovim/lua/lsp/telescope.lua
+++ files/neovim/lua/lsp/telescope.lua
@@ -22,6 +22,18 @@ local config = function()
     for keys, func in pairs(keybindings) do
         vim.keymap.set('n', keys, func)
     end
+
+    require('telescope').setup {
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        }
+      }
+    }
+    require("telescope").load_extension("fzf")
 end
 
 return {
