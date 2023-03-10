0b4ee8f9ec1d6946ca6ce175ab61184676661c3a
Davide Forlani <davide.forlani@overweb.it>
2023-03-10 10:12:44 +0100

    Added options for markdown-preview.

--- files/neovim/lua/visual/markdown.lua
+++ files/neovim/lua/visual/markdown.lua
@@ -1,4 +1,12 @@
+local options = {
+    mkdp_page_title = "${name}",
+    mkdp_theme = "light",
+}
+for key, value in pairs(options) do
+    vim.g[key] = value
+end
+
 return {
     "iamcco/markdown-preview.nvim",
-    build = function() vim.fn["mkdp#util#install"]() end,
+    build = function() vim.fn["mkdp#util#install"]() end
 }
