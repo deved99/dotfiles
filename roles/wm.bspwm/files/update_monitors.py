7d20f88918c178ea91eef355feae5ec9c64526c7
Davide Forlani <forla.df@gmail.com>
2023-03-19 10:55:42 +0100

    Added BSPWM.

---
+++ roles/wm.bspwm/files/update_monitors.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import json
+from os import environ
+import subprocess
+
+import jinja2
+
+CONFIG_DIR = environ.get("XDG_CONFIG_DIR") or f"{environ['HOME']}/.config"
+EWW_CONFIG_PATH = f"{CONFIG_DIR}/eww/eww.yuck"
+
+EWW_TEMPLATE_STR = """
+(include "./layout.yuck")
+
+{% for monitor in monitors %}
+(defwindow {{ monitor }}
+  :monitor "{{ monitor }}"
+  :stacking "bg"
+  :windowtype "dock"
+  :reserve (struts :distance "3%" :side "top")
+  :geometry (geometry :width "100%" :height "3%" :anchor "top center")
+  (layout :monitor "{{ monitor }}"))
+
+{% endfor %}
+"""
+EWW_TEMPLATE = jinja2.Template(EWW_TEMPLATE_STR)
+
+
+def update_grid():
+    subprocess.run(["bspwm-grid", "init"])
+
+def update_eww(monitors: set[str]):
+    rendered = EWW_TEMPLATE.render(monitors=monitors)
+    with open(EWW_CONFIG_PATH, "w") as f:
+        f.write(rendered)
+    subprocess.run(["eww", "--restart",  "open-many"]+list(monitors))
+
+def update_monitors(monitors: set[str]):
+    update_grid()
+    update_eww(monitors)
+
+def main():
+    cmd = subprocess.Popen(["status-watcher", "monitor"], stdout=subprocess.PIPE)
+    if cmd.stdout is None:
+        print("I expected stdout to not be None!")
+        exit(1)
+    monitors = set()
+    for line_bytes in iter(cmd.stdout.readline, ""):
+        line = line_bytes.decode("utf-8").strip()
+        new_monitors = set(json.loads(line))
+        if new_monitors != monitors:
+            print(f"Changed: {monitors} => {new_monitors}")
+            monitors = new_monitors
+            update_monitors(monitors)
+
+if __name__ == "__main__":
+    main()
